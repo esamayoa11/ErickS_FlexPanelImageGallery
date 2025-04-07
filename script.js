dceaea6aa8dc9a85ae85f24d280480f9206fe1ce
Erick Samayoa <ericksamayoa11@gmail.com>
2025-04-07 15:28:22 -0700

    First pass at setting up css and Jscript using custom images.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const panels = document.querySelectorAll('.panel')
 
+// Toggle open class
+function toggleOpen() {
+    // Close all other panels
+    panels.forEach(panel => {
+        if (panel != this) panel.classList.remove('open', 'open-active');
+    });
+    this.classList.toggle('open');
+}
+
+// Toggle open-active when transition ends
+function toggleActive(e)
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+// Add event listeners
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitioned', toggleActive));
